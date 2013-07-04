f93d501fe57634e01ff0a89472f8a39248383b04
Marlonlom <malm.gm@gmail.com>
2013-07-04 16:24:18 -0500

    javascript update

--- libs/indqm.app.js
+++ libs/indqm.app.js
@@ -8,6 +8,7 @@ indq.initialize = function () {
     this.templates.home = Handlebars.compile($("#hbt-home").html());
     this.templates.indqsList = Handlebars.compile($("#hbt-indiqs-list").html());
     this.templates.searchs = Handlebars.compile($("#hbt-search-results").html());
+    this.templates.mapping = Handlebars.compile($("#hbt-mapping").html());
 };
 indq.isOffline = function () {
     var connectionType = navigator.connection ? navigator.connection.type : null;
@@ -131,7 +132,64 @@ indq.showIndicatorsListView = function (hash) {
     $('li.item-li').addClass('mpios-li').each(function (index) {
         var odd = index % 2 === 0;
         $(this).addClass(odd ? 'mpios-odd-li' : 'mpios-even-li');
-    });
-    
+    });    
     indq.handleDANEWebpageAccess();
+};
+indq.prepareMap = function (indicator) {
+    if (indicator) {
+        var mapConfig = $.grep(mapdata, function (item, index) {
+            return item['indq'] === indicator['cod'];
+        })[0];
+        if (mapConfig !== undefined) {
+
+            var mapOptions = {
+                disableDoubleClickZoom: true,
+                streetViewControl: false,
+                scaleControl : false,
+                zoom: 7,
+                draggable : false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var map = new google.maps.Map(document.getElementById("map-canvas"),
+                mapOptions);
+            
+            var mapsEngineLayer = new google.maps.visualization.MapsEngineLayer({
+                mapId: mapConfig.map_id,
+                layerKey: 'layer_00001',
+                map: map,
+                clickable: false,
+                suppressInfoWindows: true
+            });
+        } else {
+            indq.showAlert('No hay datos para mostrar.', 'Cargar mapa');
+        }
+    }
+};
+indq.prepareMappingView = function (btn) {
+    var availabilityErrorMessage = indq.checkGoogleMapsAvailability();
+    if (availabilityErrorMessage != "") {
+        indq.showAlert(availabilityErrorMessage, 'Cargar mapa');
+    } else {
+        var cod = btn.attr('data-itm-cod') || 'nah';
+        var indiqsFound = $.grep(data['indiqs'], function (item, index) {
+            return item['cod'] === cod;
+        });
+
+        if (indiqsFound !== null && indiqsFound.length > 0) {
+            var context = {
+                indicatorTitle: indiqsFound !== null && indiqsFound.length > 0 ? indiqsFound[0].nom : ''
+            };
+            $('body').html(this.templates.mapping(context));
+            indq.prepareCommonPageBehaviour();
+            $('body').on(indq.toggleClickEvent(), 'img.back-home-icon', function (e) {
+                e.preventDefault();
+                indq.atHome = true;
+                indq.prepareMainView();
+            });
+            indq.prepareMap(indiqsFound[0]);
+        }else{
+            indq.showAlert('No hay datos para mostrar.', 'Cargar mapa');
+        }
+    }
 };
